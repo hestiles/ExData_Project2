f93d9c3b0df493e502ce5df1575945b7920652f3
Hope E Stiles <hope_stiles@bshsi.org>
2019-04-09 13:48:20 -0400

    Filtered to Baltimore, Summed Emissions per year, Printed to png

--- plot2.R
+++ plot2.R
@@ -3,3 +3,16 @@ unzip("emissions.zip")
 
 NEI<-readRDS("summarySCC_PM25.rds")
 SCC<-readRDS("Source_Classification_Code.r")
+
+
+library(dplyr)
+##Filter to Baltimore
+Bal<- filter(NEI, fips=="24510")
+
+##sum by year
+Bal2<-aggregate(Bal['Emissions'], by=Bal['year'], sum)
+
+##Create PNG file
+png(file="plot2.png", width=480, height=480)
+with(Bal2, plot(year, Emissions))
+dev.off()
