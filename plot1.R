c85f60903ca5c1d09ec3c2a147467596d7f16607
Hope E Stiles <hope_stiles@bshsi.org>
2019-04-09 12:51:48 -0400

    Download, unzip, read data in.  Create plot with year along X and Emissions along Y.

---
+++ plot1.R
@@ -0,0 +1,7 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile="emissions.zip")
+unzip("emissions.zip")
+
+NEI<-readRDS("summarySCC_PM25.rds")
+SCC<-readRDS("Source_Classification_Code.r")
+
+plot(NEI$year, NEI$Emissions)
