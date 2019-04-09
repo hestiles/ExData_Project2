0e92da3ae1cef063bf096c6b981bc45a248b4c49
Hope E Stiles <hope_stiles@bshsi.org>
2019-04-09 13:30:03 -0400

    Tried to group data by year and plot.  Got same value for all years.

--- plot1.R
+++ plot1.R
@@ -5,3 +5,10 @@ NEI<-readRDS("summarySCC_PM25.rds")
 SCC<-readRDS("Source_Classification_Code.r")
 
 plot(NEI$year, NEI$Emissions)
+##Data by year
+library(dplyr)
+NEIbyYear<-NEI %>%
+        group_by(year, sum(Emissions))
+
+with(NEIbyYear, plot(year, sum(Emissions)))
+     
