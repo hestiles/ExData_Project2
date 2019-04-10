490bc4ee09e4c19fa5d846067c53e54498358907
Hope E Stiles <hope_stiles@bshsi.org>
2019-04-10 13:32:39 -0400

    Filter data to Baltimore and LA.  This seems to work.  Identify Moter Vehicle Codes.  Filter data to Motor Vehicle Codes--this seems to cause a problem.

--- plot6.R
+++ plot6.R
@@ -3,3 +3,30 @@ unzip("emissions.zip")
 
 NEI<-readRDS("summarySCC_PM25.rds")
 SCC<-readRDS("Source_Classification_Code.rds")
+
+library(dplyr)
+##Filter to Baltimore and LA
+LABAL<- filter(NEI, fips=="24510" | fips=="06037")
+
+##Identify Motor Vehicles
+library(dplyr)
+SCC<-as_tibble(SCC)
+MV<-filter(SCC, SCC.Level.One =="Mobile Sources")
+
+library(stringr)
+MV2<-filter(MV, str_detect(SCC.Level.Two, "Vehicle"))
+
+##Identify vehicle codes
+codes<-unique(MV2$SCC)
+
+##Filter data to rows for motor vehicles
+LABALMV<-filter(LABAL, SCC==codes)
+
+##Sum by year
+LABALMV2<-aggregate(Emissions~year + fips,LABALMV, sum)
+
+library(ggplot2)
+##Create PNG file
+png(file="plot6.png", width=480, height=480)
+qplot(year, Emissions, data=LABALMV2, facets=.~ fips)+geom_smooth()
+dev.off()
