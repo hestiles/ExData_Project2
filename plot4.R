c0b84e65e23f77d8e12331f78d8242582c1d62ab
Hope E Stiles <hope_stiles@bshsi.org>
2019-04-10 08:36:39 -0400

    Filtered to Coal combustion sources

--- plot4.R
+++ plot4.R
@@ -2,9 +2,12 @@ download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zi
 unzip("emissions.zip")
 
 NEI<-readRDS("summarySCC_PM25.rds")
-SCC<-readRDS("Source_Classification_Code.r")
+SCC<-readRDS("Source_Classification_Code.rds")
 
 ##Filter to just coal combustion
-SCC2<-filter(SCC, )
 library(dplyr)
-coal<-filter(NEI, )
+SCC<-as_tibble(SCC)
+SCC2<-filter(SCC, EI.Sector=="Fuel Comb - Comm/Institutional - Coal")
+
+
+
